26fc31759cdc4226de368359567ab019a976bc8f
rubberducky <mcrubygirl@hotmail.com>
2024-12-10 18:01:34 +0100

    Added MenuChoices.

--- POKEMONN/Program.cs
+++ POKEMONN/Program.cs
@@ -7,10 +7,20 @@ namespace POKEMONN
         public static string? FileName = "pokemon.txt";
         static void Main(string[] args)
         {
+            string[] _menu = {
+                "1. Add Pokemon", 
+                "2. See Pokemon-list", 
+                "3. Find Pokemon", 
+                "4. Edit Pokemon", 
+                "5. Delete Pokemon",
+                "6. Exit Program"};
             new FileHandler(FileName);
-            FileHandler.CreateFile();
             new DrawMenu(1, 1, 80, 25, "Pokemon Index");
+            new MenuChoices(_menu);
+            FileHandler.CreateFile();
+            MenuChoices.SetupMenuChoices();
             DrawMenu.MakeMenu();
+
             Console.ReadKey();
         }
     }
---
+++ POKEMONN/utils/MenuChoices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POKEMONN.utils
+{
+    class MenuChoices
+    {
+        public static string[] Entries = { };
+
+        public MenuChoices(string[] _entries)
+        {
+            Entries = _entries;
+        }
+
+        public static void SetupMenuChoices()
+        {
+            for (int i = 0; i < Entries.Length; i++)
+            {
+                Console.SetCursorPosition(DrawMenu.X + 2, DrawMenu.Y + 2 + i);
+                Console.WriteLine(Entries[i]);
+            }
+        }
+    }
+}
